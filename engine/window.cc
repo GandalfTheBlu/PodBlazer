28425e37cd662e9b2e084e32bc9ac7b49d713a14
Jgren <93703319+Jgren@users.noreply.github.com>
2023-05-10 12:14:26 +0200

    changed for opengl es

--- engine/window.cc
+++ engine/window.cc
@@ -63,12 +63,18 @@ namespace Engine
 			return false;
 		}
 
-		glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
 		glfwWindowHint(GLFW_RED_BITS, 8);
 		glfwWindowHint(GLFW_GREEN_BITS, 8);
 		glfwWindowHint(GLFW_BLUE_BITS, 8);
 		glfwWindowHint(GLFW_SRGB_CAPABLE, GL_TRUE);
-		glfwWindowHint(GLFW_SAMPLES, 8);
+		//glfwWindowHint(GLFW_SAMPLES, 4);
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+		//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+		glewExperimental = true;
 
 		width = _width;
 		height = _height;
